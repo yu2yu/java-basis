d03ec8f91e55afd3c8d1f3931d21274d2ad56a51
yu2yu <youny1118@gmail.com>
2020-12-22 10:37:03 +0800

    两个数组的并集

---
+++ src/main/java/com/yy/algorithm/array/TwoArrayInter.java
@@ -0,0 +1,22 @@
+package com.yy.algorithm.array;
+
+/**
+ * @version 1.0
+ * @description: 两个数组的交集
+ *                  其实就是
+ * @author: yy
+ * @date: 2020/12/22
+ */
+public class TwoArrayInter {
+
+
+    public void intersect(int[] num1,int[] num2){
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
